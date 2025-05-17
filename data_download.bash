0693b2d176077bcc7c51d67ae33f499473ad8220
DavitGamtenadze <gamtenadze.david@gmail.com>
2025-05-17 21:27:27 +0400

    GDP data

--- data_download.bash
+++ data_download.bash
@@ -1,5 +1,6 @@
 # Quarterly GDP data from AskGov, GeoStat only had data from 2010
-curl -L -o georgia_quarterly_gdp.xlsx "https://askgov.ge/ka/request/2971/response/4047/attach/8/.xlsx?cookie_passthrough=1"
+#curl -L -o georgia_quarterly_gdp.xlsx "https://askgov.ge/ka/request/2971/response/4047/attach/8/.xlsx?cookie_passthrough=1"
+curl -L -o georgia_quarterly_gdp.xlsx "https://geostat.ge/media/69463/03_GDP-at-Current-Prices.xlsx"
 
 # Nominal effective exchange rate, monthly data
 curl -L -o georgia_effective_nominal_exchange_rate.xlsx "https://nbg.gov.ge/fm/%E1%83%A1%E1%83%A2%E1%83%90%E1%83%A2%E1%83%98%E1%83%A1%E1%83%A2%E1%83%98%E1%83%99%E1%83%90/exchange_rates/eng/nominal-effective-exchange-rateeng.xlsx?v=y73wd"
@@ -13,6 +14,8 @@ curl -L -o georgia_monthly_inflation_2000_2003.xlsx "https://geostat.ge/media/54
 # Monthly inflation data from 2004 to 2025 / already in percentages needs to be divided by 100
 curl -L -o georgia_monthly_inflation_2004_2025.xlsx "https://cpd-iset-ba-thesis.s3.eu-north-1.amazonaws.com/Monthly_inflation_2004_2025.xlsx"
 
+# Monthly YoY growth rates from 2011
+curl -L -o georgia_monthly_yoy_growth_rates_2011_2024.xlsx "https://cpd-iset-ba-thesis.s3.eu-north-1.amazonaws.com/Rapid_Economic_Growth_Estimates_2011-2025.xlsx"
 
 # Move all files ending with .xlsx to data/preliminary_data
 mv *.xlsx data/preliminary_data/
